d3391f0a57b5b02df35e77e915f1d32889b3c1da
victor kweyu <wkweyu@yahoo.co.uk>
2026-02-15 21:30:47 +0300

    Fix missing movement controller files for migration

---
+++ parishmis/parish_management_system/doctype/movement/movement.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2024, Parish Management System and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+class Movement(Document):
+	pass
---
+++ parishmis/parish_management_system/doctype/movement_member/movement_member.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Parish Management System and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Movement Member', {
+	// refresh: function(frm) {
+
+	// }
+});
